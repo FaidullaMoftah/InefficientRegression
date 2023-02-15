7c0ef6eecc585ca4e6e230610f9ae162352acb1c
Faidullah Mofath <faidullamoftah2@gmail.com>
2023-02-15 02:53:32 +0200

    path was hardcoded

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include "polyreg.cpp"
 int main() {
-    polyreg poly = *new polyreg("C:\\Users\\faidu\\Desktop\\polyreg\\data.csv", 1, false, 4);
+    //edit path
+    string path = "";
+    polyreg poly = *new polyreg(path, 1, false, 4);
     poly.X.print();
     poly.y.print();
     poly.fit();
